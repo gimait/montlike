dc3cbe803ad2dd5b95101b6c5c602deb2ff45ac6
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-02-11 11:32:00 +1100

    Tutorial's third chapter: dungeon generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -15,6 +17,10 @@ const COLOR_DARK_GROUND: Color = Color {
     b: 150,
 };
 
+//parameters for dungeon generator
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 #[derive(Clone, Copy, Debug)]
 struct Tile {
@@ -38,6 +44,100 @@ impl Tile {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map(player: &mut Object) -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - w);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            create_room(new_room, &mut map);
+
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            rooms.push(new_room)
+        }
+    }
+
+    map
+}
 
 struct Tcod {
     root: Root,
@@ -54,7 +154,7 @@ struct Object {
 
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Object { x, y, char, color}
+        Object { x, y, char, color }
     }
 
     pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
@@ -74,13 +174,6 @@ type Map = Vec<Vec<Tile>>;
 
 struct Game {
     map: Map,
-}
-
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
-    map
 }
 
 fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
@@ -106,13 +199,11 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
         &mut tcod.root,
         (0, 0),
         1.0,
-        1.0
+        1.0,
     );
 }
 
-
 fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &Game) -> bool {
-
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = tcod.root.wait_for_keypress(true);
@@ -151,27 +242,23 @@ fn main() {
     let mut tcod = Tcod { root, con };
 
     // Player
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let player = Object::new(0, 0, '@', WHITE);
     // NPC
     let npc = Object::new(SCREEN_WIDTH / 2 + 1, SCREEN_HEIGHT / 2 + 1, '@', YELLOW);
 
     let mut objects = [player, npc];
 
-    let game = Game {
-        map: make_map(),
-    };
+    let game = Game { map: make_map(&mut objects[0]) };
 
     while !tcod.root.window_closed() {
         tcod.con.clear();
+        render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
 
         let player = &mut objects[0];
         let exit = handle_keys(&mut tcod, player, &game);
         if exit {
             break;
         }
-        render_all(&mut tcod, &game, &objects);
-
     }
 }
